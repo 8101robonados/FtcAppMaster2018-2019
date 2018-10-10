93d3a01300bba7d9b4506ae69fe14c77f60965a8
FTC computer <8101robonados@gmail.com>
2018-10-10 18:26:41 -0500

    An uplode of all the files

---
+++ FTC_2018-2019/ftc_app-master/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/arcadeDrive.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name="arcadeDrive", group="Linear Opmode")
+public class arcadeDrive extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftDrive;
+    private DcMotor rightDrive;
+    private DcMotor armMotor;
+
+    @Override
+    public void runOpMode()
+    {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Initialize hardware variables.
+        leftDrive  = hardwareMap.get(DcMotor.class, "leftDrive");
+        rightDrive = hardwareMap.get(DcMotor.class, "rightDrive");
+        armMotor = hardwareMap.get(DcMotor.class, "arm");
+
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+        armMotor.setDirection(DcMotor.Direction.FORWARD);
+
+        waitForStart();
+        runtime.reset();
+
+        while (opModeIsActive())
+        {
+
+            double stickY = -gamepad1.left_stick_y;
+            double stickX = gamepad1.right_stick_x;
+
+            double leftPower = stickY+stickX; //stickX to the left is -
+            double rightPower = stickY-stickX;
+
+            leftPower    = Range.clip(leftPower, -1.0, 1.0) ;
+            rightPower   = Range.clip(rightPower, -1.0, 1.0) ;
+
+            //sets power
+            leftDrive.setPower(leftPower);
+            rightDrive.setPower(rightPower);
+            armMotor.setPower(gamepad2.left_stick_y);
+
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.update();
+        }
+    }
+}
+
